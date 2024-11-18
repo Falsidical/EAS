8bc58f5ec1ef1386db4949562b2be294483eae8c
Falsidical <franport@outlook.com>
2024-11-18 20:06:25 +0000

    Hover + Rainbow

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const grid = document.querySelector('.grid-container');
+const slider = document.querySelector('#slider');
+const sliderLabel = document.querySelector('#sliderLabel');
 
+let rainbowMode = true;
+
+slider.addEventListener('input', (e) => {
+  sliderLabel.innerText = `Size ${e.target.value}x${e.target.value}`;
+  drawGrid(e.target.value);
+});
 function drawCell() {
   const cell = document.createElement('div');
+  cell.addEventListener('mouseover', () => {
+    if (rainbowMode) {
+      cell.style.backgroundColor = randomColor();
+    } else {
+      cell.style.backgroundColor = 'red';
+    }
+  });
   return cell;
 }
 
 function drawGrid(size) {
   if (size < 1) return;
+  grid.replaceChildren();
   for (let i = 0; i < size; i++) {
     const row = document.createElement('div');
     row.classList.add('row');
@@ -16,6 +32,13 @@ function drawGrid(size) {
     }
     grid.appendChild(row);
   }
+}
+
+function randomColor() {
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+  return `rgb(${red}, ${green}, ${blue})`;
 }
 
 drawGrid(16);
