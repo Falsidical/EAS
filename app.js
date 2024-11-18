b4ec2a0eaecb0b26fb848439c4c5fdc33ab8afa7
Falsidical <franport@outlook.com>
2024-11-18 23:57:46 +0000

    Add Progressive Darkening Effect

--- app.js
+++ app.js
@@ -2,15 +2,21 @@ const grid = document.querySelector('.grid-container');
 const slider = document.querySelector('#slider');
 const sliderLabel = document.querySelector('#sliderLabel');
 const rainbowToggle = document.querySelector('#rainbow');
+const gradientToggle = document.querySelector('#gradient');
 const colorSelector = document.querySelector('#color');
 const clearGridBtn = document.querySelector('#clearGrid');
 
 let rainbowMode = rainbowToggle.checked;
+let gradientMode = gradientToggle.checked;
 let activeColor = colorSelector.value;
 let gridSize = slider.value;
 
 rainbowToggle.addEventListener('change', (e) => {
   rainbowMode = !rainbowMode;
+});
+
+gradientToggle.addEventListener('change', (e) => {
+  gradientMode = !gradientMode;
 });
 
 colorSelector.addEventListener('change', (e) => {
@@ -28,9 +34,12 @@ clearGridBtn.addEventListener('click', () => {
 });
 function drawCell() {
   const cell = document.createElement('div');
+  cell.style.opacity = 1;
   cell.addEventListener('mouseover', () => {
     if (rainbowMode) {
       cell.style.backgroundColor = randomColor();
+    } else if (gradientMode) {
+      if (cell.style.opacity >= 0.1) cell.style.opacity = cell.style.opacity - 0.1;
     } else {
       cell.style.backgroundColor = activeColor;
     }
